11202443c787db686afd8b838b32783b9e6af365
Astrale <ash4567@outlook.fr>
2023-09-28 21:02:53 +0200

    fix bug in n mode + export html

--- scenes/runners/n_runner.gd
+++ scenes/runners/n_runner.gd
@@ -4,7 +4,7 @@ var x := preload("res://scenes/main/main.tscn").instance()
 
 func _ready():
 	x.parameters = $"/root/Configsaveload".n_up
-	x.parameters.delay = 0.1
+#	x.parameters.delay = 0.1
 	x.connect("EndGame",self,"reload")
 	add_child(x)
 
